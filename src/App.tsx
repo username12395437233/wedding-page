5a30349d48c4de99f42d96e0d3bb7246897a5135
Aleksandr Zarubin <zarubin_14@list.ru>
2026-02-10 14:47:10 +0300

    chore: add basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Button } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <Tooltip>
+        <TooltipTrigger>Hover</TooltipTrigger>
+        <TooltipContent>
+          <p>Add to library</p>
+        </TooltipContent>
+      </Tooltip>
     </div>
   )
 }
