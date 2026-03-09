5d0693a0d91c4cbf4888ef55fde1f561bb0b30e1
Alexandr Zarubin <zarubin_14@list.ru>
2026-03-09 22:30:52 +0300

    fix: correct build v1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/KAWedding/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
